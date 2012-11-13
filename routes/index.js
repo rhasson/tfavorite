52d81044ab301ce9da0ec45949d65bd9acbceee3
Roy Hasson <rhasson@gmail.com>
2012-11-13 14:28:19 -0500

    base twitter logging and signing code

---
+++ routes/index.js
@@ -0,0 +1,68 @@
+/*
+*	Routes implementation
+*	v.0.0.1
+*/
+
+var r = require('request'),
+		qs = require('querystring'),
+		config = require('./config').config;
+
+exports.routes = {
+	index: function(req, res, next) {
+
+	},
+	auth_cb: function(req, res, next) {
+				var v = qs.parse(body);
+				req.session.access_token.oauth_verifier = body.oauth_verifier;
+				var oauth = {
+							consumer_key: config.twitter.consumer_key,
+							consumer_secret: config.twitter.consumer_secret,
+							token: req.session.access_token.oauth_token,
+							verifier: req.session.access_token.oauth_verifier,
+							token_secret: req.session.access_token.oauth_token_secret
+						},
+						u = config.twitter.base_url + '/oauth/access_token';
+				r.post({url: u, oauth: oauth}, function(err, resp, body) {
+					if (!err && resp.statusCode === 200) {
+						req.session.access_token = qs.parse(body);
+						req.session.oauth = {
+							consumer_key: config.twitter.consumer_key,
+							consumer_secret: config.twitter.consumer_secret,
+							token: req.session.access_token.oauth_token,
+							token_secret: req.session.access_token.oauth_token_secret
+						};
+						var u = config.twitter.base_url + '/1/users/show.json?',
+								params = {
+									screen_name: req.session.access_token.screen_name,
+									user_id: req.session.access_token.user_id
+								};
+						u += qs.stringify(params);
+						r.get({url: u, oauth: req.session.oauth, json: true}, function(err2, resp2, user) {
+							if (!err2 && resp2.statusCode === 200) {
+								console.log(user);
+							}
+						});
+					}
+				});
+	}, 
+	twitter_login: function(req, res, next) {
+		var oauth = {
+			callback: config.home_url + config.twitter.callback,
+			consumer_key: config.twitter.consumer_key,
+			consumer_secret: config.twitter.consumer_secret
+		},
+		u = config.twitter.base_url + '/oauth/request_token';
+
+		r.post({url: u, oauth: oauth}, function(err, resp, body) {
+			if (!err && resp.statusCode === 200) {
+				req.session.access_token = qs.parse(body);
+
+				res.redirect(
+					config.twitter.base_url + 
+					'/oauth/authenticate?' + 
+					qs.stringify(oauth_token: req.session.access_token.oauth_token));
+			}
+		});
+	}
+}
+
---
+++ server.js
@@ -0,0 +1,32 @@
+/*
+*  favio.us server implementation
+*  v.0.0.1
+*/
+
+var Express = require('express'),
+		server = Express(), 
+		routes = require('./routes').routes;
+		//RedisStore = require('connect-redis')(Express);
+
+/* Server Configuration */
+server.configure(function(){
+  server.use(express.logger());
+  server.set('views', __dirname + '/views');
+  server.set('view engine', 'jade');
+  server.use(express.bodyParser());
+  server.use(express.cookieParser());
+  server.use(express.session({secret: 'testing this stuff');//, store: new RedisStore}));
+  server.use(express.methodOverride());
+  server.use(server.router);
+  server.use(express.static(__dirname + '/public'));
+  server.use(express.errorHandler({showStack: true, dumpExceptions: true}));
+});
+
+server.configure('development', function(){
+  server.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+server.configure('production', function(){
+  server.use(express.errorHandler());
+});
+
